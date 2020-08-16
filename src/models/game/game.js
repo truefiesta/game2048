b04f99546bb7b8e054fde8efc05c5eae8387df90
Yulia Karpova <karpova.jul@gmail.com>
2020-08-16 20:02:15 +1000

    Add methods to the Game class to start a game and make a move

--- src/models/game/game.js
+++ src/models/game/game.js
@@ -12,13 +12,54 @@ const BoardSizeToInitialFilledTilesNumber = {
 };
 
 class Game {
-    constructor() {
-        this._score = 0;
+    constructor(boardSize) {
+        this._start(boardSize);
+    }
+
+    getStep() {
+        return this._step;
     }
 
     getScore() {
         return this._score;
     }
+
+    setScore(newScore) {
+        this._score = newScore;
+    }
+
+    setBoardValues(values) {
+        this._board.setMatrix(values);
+    }
+
+    getBoardValues() {
+        return this._board.getMatrix();
+    }
+
+    _updateScore(newPoints) {
+        this._score += newPoints;
+    }
+
+    moveTiles(direction) {
+        if (this._board) {
+            if (this._board.hasEmptyTiles()) {
+                const points = this._board.moveTiles(direction);
+                this._updateScore(points);
+                const newValueForEmptyTile = generateRandomTileValue(this._step);
+                this._board.setValueOnRandomEmptyTile(newValueForEmptyTile);
+            } else if (this._board.hasTilesToMerge()) {
+                const points = this._board.moveTiles(direction);
+                this._updateScore(points);
+                if (this._board.hasEmptyTiles()) {
+                    const newValueForEmptyTile = generateRandomTileValue(this._step);
+                    this._board.setValueOnRandomEmptyTile(newValueForEmptyTile);
+                }
+            } else {
+                this._end();
+            }
+        }
+    }
+
     _getNumberOfTilesForInitialFill() {
         let numberOfTilesToFill;
         if (this._size <= BoardSize.SMALL_BOARD) {
@@ -39,6 +80,22 @@ class Game {
 
         return initialNonEmptyTileValues;
     }
+
+    _initializeBoard(boardSize) {
+        const initialNonEmptyTileValues = this._getInitialValuesForNonEmptyTiles();
+        this._board = new Board(boardSize, initialNonEmptyTileValues);
+    }
+
+    _start(boardSize) {
+        this._score = 0;
+        this._step = 0;
+        this._initializeBoard(boardSize);
+    }
+
+    _end() {
+
+    }
 }
 
+export {BoardSize, BoardSizeToInitialFilledTilesNumber};
 export default Game;
--- src/models/game/game.test.js
+++ src/models/game/game.test.js
@@ -1,9 +1,77 @@
 import Game from "./game";
+import {Move} from "../../const";
+import {calcMatrixSum} from "../board/utils";
 
 describe(`Game class`, () => {
+    let game;
+    const size = 4;
+    beforeEach(() => {
+        game = new Game(size);
+    })
+    
     it(`can create a new game`, () => {
-        const game = new Game();
-
+        expect(game.getStep()).toEqual(0);
         expect(game.getScore()).toEqual(0);
     });
+
+    it(`sets new score`, () => {
+        const newScore = 128;
+        game.setScore(newScore);
+        expect(game.getScore()).toEqual(newScore);
+    });
+
+    it(`moves tiles down and updates the score when tiles merge`, () => {
+        const initialMatrix = [
+            [4, 0, 0, 0],
+            [2, 2, 2, 2],
+            [4, 4, 4, 0],
+            [8, 0, 8, 0]
+        ];
+
+        game.setBoardValues(initialMatrix);
+        expect(game.getScore()).toEqual(0);
+        game.moveTiles(Move.DOWN);
+        expect(game.getScore()).toEqual(32);
+    });
+
+    describe(`there are no empty tiles on the board but some tiles can be merged down or up`, () => {
+        const initialMatrix = [
+            [1, 5, 9, 13],
+            [2, 2, 10, 14],
+            [3, 7, 11, 15],
+            [4, 8, 12, 16]
+        ];
+        it(`updates the score when recieves a down move`, () => {
+            game.setBoardValues(initialMatrix);
+            expect(game.getScore()).toEqual(0);
+            game.moveTiles(Move.DOWN);
+            expect(game.getScore()).toEqual(4);
+
+            const initialMatrixSum = calcMatrixSum(initialMatrix);
+            expect(calcMatrixSum(game.getBoardValues())).toBeGreaterThan(initialMatrixSum);
+        });
+
+        it(`updates the score when recieves a up move`, () => {
+            game.setBoardValues(initialMatrix);
+            expect(game.getScore()).toEqual(0);
+            game.moveTiles(Move.UP);
+            expect(game.getScore()).toEqual(4);
+
+            const initialMatrixSum = calcMatrixSum(initialMatrix);
+            expect(calcMatrixSum(game.getBoardValues())).toBeGreaterThan(initialMatrixSum);
+        });
+
+        it(`doesn't change the score when recieves a left or right move`, () => {
+            game.setBoardValues(initialMatrix);
+            expect(game.getScore()).toEqual(0);
+            game.moveTiles(Move.RIGHT);
+            expect(game.getScore()).toEqual(0);
+            game.moveTiles(Move.LEFT);
+            expect(game.getScore()).toEqual(0);
+
+            const initialMatrixSum = calcMatrixSum(initialMatrix);
+            expect(calcMatrixSum(game.getBoardValues())).toEqual(initialMatrixSum);
+        });
+    });
+
 });
