5027eab756138cf48b065cae00b0aae132c83776
Yulia Karpova <karpova.jul@gmail.com>
2020-08-20 20:34:01 +1000

    Import components in index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, {Fragment} from "react";
 import ReactDOM from "react-dom";
-import Board from "./components/board";
+import BoardSize from "./components/board-size/board-size";
+import Board from "./components/board/board";
+import NewGameButton from "./components/new-game-button/new-game-button";
+import Score from "./components/score/score";
+import {ScoreType} from "./const";
 import './css/styles.css';
 
-console.log('Hi!');
+const tileValues = [0, 0, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 2, 2];
 
 const App = () => {
     return (
-        <Board/>
+        <Fragment>
+            <section className="new-game-container box">
+                <BoardSize />
+                <NewGameButton />
+            </section>
+            <section className="score">
+                <Score
+                    title={ScoreType.GAME}
+                    points={0}
+                />
+                <Score
+                    title={ScoreType.BEST}
+                    points={0}
+                />
+            </section>
+            <Board
+                tileValues={tileValues}
+            />
+        </Fragment>
     );
 };
 
-ReactDOM.render(<App />, document.getElementById('board'));
+ReactDOM.render(<App />, document.getElementById('game'));
